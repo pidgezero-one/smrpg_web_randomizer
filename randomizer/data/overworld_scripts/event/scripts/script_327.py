6065840521e78e65316a998d7f0b94bfd1ec2f4b
stef kischak <s.kischak@gmail.com>
2026-01-14 14:02:36 -0500

    checkpoint

--- randomizer/data/overworld_scripts/event/scripts/script_327.py
+++ randomizer/data/overworld_scripts/event/scripts/script_327.py
@@ -31,6 +31,6 @@ from ....items import *
 from ....packets import *
 
 script = EventScript([
-	RunDialog(dialog_id=DI2298_EMPTY, above_object=MEM_70A8, closable=True, sync=False, multiline=True, use_background=True),
+	RunDialog(dialog_id=DI2298_TOAD, above_object=MEM_70A8, closable=True, sync=False, multiline=True, use_background=True),
 	Return()
 ])
