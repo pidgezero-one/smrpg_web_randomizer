274661bc44d5c016ac6f9dc5fe2245639fb95748
pidgezero_one <s.kischak@gmail.com>
2026-06-07 23:52:42 -0400

    fix punchinello bobombs attacking wrong targets, fix mushrom derby softlock, fix infinite coin chest in rose town shop

--- randomizer/data/rooms/room_87.py
+++ randomizer/data/rooms/room_87.py
@@ -28,7 +28,7 @@ room = Room(
                 index_in_main_buffer=True
             ),
             Buffer(
-                buffer_type=BufferType.EMPTY_3,
+                buffer_type=BufferType.COINS,
                 main_buffer_space=BufferSpace.BYTES_0,
                 index_in_main_buffer=True
             )
